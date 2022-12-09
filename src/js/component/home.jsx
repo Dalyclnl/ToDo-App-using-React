b45b739881ce3bb6e23f4217e7a701d2f07f0316
Dalyclnl <dalyarmijoa@yahoo.com>
2022-12-09 18:46:11 +0000

    rev

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,37 +1,37 @@
 import React,{useState} from "react";
 
 //create your first component
+
+
+
 const Home = () => {
 	const [inputValue,SetInputValue]=useState(" ");
-	const [ToDo,setToDos ] = useState([]);
+	const [ToDos,setToDos ] = useState([]);
 	return (
 		<div className="container">
-			<h1> My ToDos</h1>
-		 <ul>
-			<li>
-				<input
-				 type="text"
-			      onChange={(e)=> SetInputValue(e.target.value)}
-				  value ={inputValue}
-				  oneKeyPress ={(e) => {
-					if (e.key==="Enter") {
-						setToDos(todos.concat([inputValue]));
-						SetInputValue (" ");
-					}
-				  }}
-			placeholder= "What shoul i do now?"></input>
-			</li>
-			{todos.map((item, index)=> (
-
-			) )}
-
-			</li>
-			
-			</ul>
-		
-		<div> 20 task </div>
-	</div>	
-	);
-};
-
-export default Home;
+				<h1> My ToDos</h1>
+				<ul>
+					<li>
+						<input
+						type="text"
+						onChange={(e)=> SetInputValue(e.target.value)}
+						value={inputValue}
+						onkeypress={(e) => {
+							if (e.key==="Enter") {
+								setToDos(ToDos.concat([inputValue]));
+								SetInputValue (" ");
+							}
+						}}
+						placeholder= "What should i do now?"> </input>
+					</li>
+					 {ToDos.map((item, index)=> (<li>{item}{""} <i class="basurar"   onClick={() => setToDos(ToDos.filter((T, currentIndex) => index != currentIndex ))
+					 </i>
+					 }</li>}</ul>	 
+						)
+					 }
+			   </ul>				   
+		</div>   
+	     <div>[ToDos.length] task </div>	
+    	);
+	};				
+ export default Home;
